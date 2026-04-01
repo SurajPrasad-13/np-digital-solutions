2d2e922e4399b4da9fa4458c0477bd41e0745300
pradeep <pradeepkumawat1310@gmail.com>
2026-04-01 14:51:34 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -12,8 +13,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        heading: ['"Space Grotesk"', 'sans-serif'],
-        body: ['Inter', 'sans-serif'],
+        heading: ['Vidaloka', 'serif'],
+        body: ['Vidaloka', 'serif'],
       },
       colors: {
         border: "hsl(var(--border))",
@@ -62,5 +63,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } satisfies Config;
